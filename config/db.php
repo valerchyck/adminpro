d8bb12009931a7b73c9b2883453ccf4651207789
valerchyck <valerchyck937@gmail.com>
2016-11-14 09:53:58 +0200

    Update db.php

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=adminpo',
-    'username' => 'v10790_dbagent',
-    'password' => '1v7-LA!_2d',
+    'username' => 'root',
+    'password' => 'root',
     'charset' => 'utf8',
 ];
